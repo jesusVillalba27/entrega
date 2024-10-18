a53f9fdec3184a49465f5ea67de7fe4589520f07
LAPTOP-A0MQDKN7\57311 <jesus.villalbav@cecar.edu.co>
2024-10-18 07:14:07 -0500

    se agrego el proyecto

---
+++ entrega/entrega/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace entrega
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ entrega/entrega/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace entrega
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
